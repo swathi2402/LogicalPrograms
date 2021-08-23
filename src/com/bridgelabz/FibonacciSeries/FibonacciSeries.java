044531b23f70a9147edc24eab5cc6395b3025ef3
Swathi <swathi.h@perfios.com>
2021-08-23 19:48:05 +0530

    ADDED: Class FibonacciSeries to print the series of N numbers

---
+++ src/com/bridgelabz/FibonacciSeries/FibonacciSeries.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.FibonacciSeries;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int fibonacciLimit = 10;
+		int previousNumber = 0;
+		int currentNumber = 1;
+		System.out.print(previousNumber + "," + currentNumber);
+
+		for (int index = 2; index < fibonacciLimit; index++) {
+			int nextNumber = previousNumber + currentNumber;
+			previousNumber = currentNumber;
+			currentNumber = nextNumber;
+			System.out.print("," + nextNumber);
+		}
+	}
+}
